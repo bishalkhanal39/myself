fa2414462a96b38890dbbbb456762df5f3b3458d
bishalkhanal39 <bshl.khnl321@gmail.com>
2021-01-22 14:13:49 +0545

    first

---
+++ src/main/java/com/henry/busticketing/busticketing/BusticketingApplication.java
@@ -0,0 +1,13 @@
+package com.henry.busticketing.busticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusticketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusticketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henry/busticketing/busticketing/BusticketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.busticketing.busticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusticketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
